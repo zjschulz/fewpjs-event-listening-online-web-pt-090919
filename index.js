436c4487a3abadacf9956e9c3f1ec7da7a499511
zjschulz <zjschulz@gmail.com>
2020-03-23 18:11:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
-function addingEventListener() {
+function addingEventListener() {const input = document.getElementById('input');
+input.addEventListener('click', function(event) {
+  alert('I was clicked!');
+});
 }
